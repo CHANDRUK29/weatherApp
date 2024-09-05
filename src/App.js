23d78235dac48a6eeb73001826e95960ad0cb1b7
chandru <chandruviru143@gmail.com>
2024-09-05 22:25:24 +0530

    [WIP] | Header.js work in Progress | App.js changes done

--- src/App.js
+++ src/App.js
@@ -57,7 +57,24 @@ function App() {
         <div className="w-2/4 p-5">
           <Header />
           <div className="flex flex-col my-10">
-            {weatherData.length === 0}
+            {
+              weatherData.length === 0 ?
+                <div className="container p-4 flex items-center justify-center h-1/3 mb-auto">
+                  <div className="text-gray-300 text-4xl font-bold uppercase">{noData}</div>
+                </div> :
+                <>
+                  <h1 className="text-5xl text-gray-800 mt-auto mb-4">Today</h1>
+                  <DetailsCard weatherIcon={weatherIcon} data={weatherData} />
+                  <h1 className="text-3xl text-gray-600 mt-10 mb-4">More on {city}</h1>
+                  <ul className="grid grid-cols-2 gap-2">
+                    {weatherData.list.map((days, index) => {
+                      if (index > 0) {
+                        return (<SummaryCard key={index} day={days} />)
+                      }
+                    })}
+                  </ul>
+                </>
+            }
           </div>
         </div>
       </div>
